8d35c888436bf4544732a9aebba9ecee1ad8ceca
ageng <rizsanaa23@gmail.com>
2023-12-01 15:13:11 +0700

    menambahkan logo dan mengganti tittle setiap halaman

--- diskusi.php
+++ diskusi.php
@@ -15,7 +15,8 @@ if (empty($_SESSION['username'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Collapsible sidebar using Bootstrap 4</title>
+    <title>Bincang Kuliner : Forum Diskusi</title>
+    <link rel="shortcut icon" href="/img/logo.png" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Bootstrap CSS CDN -->
